361b949fe465cb81bd84081c73e93e6c05eee4ab
Devman <inevitabledouble.e@gmail.com>
2026-06-06 08:10:02 +0200

    import path

--- src/config/cloudinary.config.ts
+++ src/config/cloudinary.config.ts
@@ -1,6 +1,7 @@
 
 import { v2 as cloudinary } from "cloudinary"
 import { CloudinaryStorage } from "multer-storage-cloudinary"
+import * as path from "path"
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
